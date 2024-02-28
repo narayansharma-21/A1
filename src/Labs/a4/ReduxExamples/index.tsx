64600f4c2b8987c0c36a61bbedc2d0da3312a073
narayansharma-21 <97.sharman@gmail.com>
2024-02-28 17:11:00 -0500

    2.4.1 - Issues with redux

---
+++ src/Labs/a4/ReduxExamples/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HelloReducer from "./HelloRedux/helloReducer";
+import HelloRedux from "./HelloRedux";
+
+const ReduxExamples = () => {
+  return(
+    <div>
+      <h2>Redux Examples</h2>
+      <HelloRedux/>
+    </div>
+  );
+};
+
+export default ReduxExamples;
