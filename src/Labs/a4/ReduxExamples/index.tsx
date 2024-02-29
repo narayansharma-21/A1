5a48db490b92818d63dde7ea4fd28252f0667786
narayansharma-21 <97.sharman@gmail.com>
2024-02-29 15:12:19 -0500

    Finished lab

--- src/Labs/a4/ReduxExamples/index.tsx
+++ src/Labs/a4/ReduxExamples/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import HelloReducer from "./HelloRedux/helloReducer";
 import HelloRedux from "./HelloRedux";
+import CounterRedux from "./CounterRedux";
+import TodoList from "./todos/TodoList";
 
 const ReduxExamples = () => {
   return(
     <div>
       <h2>Redux Examples</h2>
       <HelloRedux/>
+      <CounterRedux/>
+      <TodoList />
     </div>
   );
 };
